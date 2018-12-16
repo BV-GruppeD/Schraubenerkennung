46233fad72bfb8469bac19a4b964aedc2d1494d9
six-two <0x62@pm.me>
2018-12-16 15:54:11 +0100

    Added dilation test code

--- src/main/java/com/bv_gruppe_d/imagej/Schrauben_Erkennung.java
+++ src/main/java/com/bv_gruppe_d/imagej/Schrauben_Erkennung.java
@@ -1,19 +1,29 @@
 package com.bv_gruppe_d.imagej;
 
+import com.bv_gruppe_d.imagej.DilateAndErode.StructureElement;
+
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
 public class Schrauben_Erkennung implements PlugInFilter {
-	
+
 	@Override
 	public void run(ImageProcessor ip) {
-		
+		DilateAndErode.color2gray(ip);
+		DilateAndErode.binarize(ip, 128);
+		StructureElement se = new StructureElement(new boolean[][] {
+			new boolean[]{false, true, false},
+			new boolean[]{true, true, true},
+			new boolean[]{false, true, false},
+		}, 1, 1);
+		for (int i = 0; i < 1; ++i) {
+			DilateAndErode.dilate(ip.duplicate(), ip, se);
+		}
 	}
 
 	@Override
 	public int setup(String arg0, ImagePlus img) {
-		return DOES_8G;
+		return DOES_ALL;
 	}
-
 }
