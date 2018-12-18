c10eebad92760bc8d121d811fd969697d0c24716
six-two <0x62@pm.me>
2018-12-18 10:17:28 +0100

    Added code for leaving just the screw

--- src/main/java/com/bv_gruppe_d/imagej/Run.java
+++ src/main/java/com/bv_gruppe_d/imagej/Run.java
@@ -5,33 +5,39 @@ import com.bv_gruppe_d.imagej.DilateAndErode.StructureElement;
 import ij.process.ImageProcessor;
 
 /**
- * Das Bild das wir haben hat den Vordergrund schwarz
- * und den Hintergrund weiss, deshalb sind opening/closing
- * und dilate/erode vertauscht ODER das bild muss vorher negiert werden
- * @author user
+ * Das Bild das wir haben hat den Vordergrund schwarz und den Hintergrund weiss,
+ * deshalb sind opening/closing und dilate/erode vertauscht ODER das bild muss
+ * vorher negiert werden
+ * 
+ * @author patrick
  *
  */
-public class Run{
+public class Run {
 	private final StructureElement edgeDetection;
 	private final StructureElement closeHoles;
-	
-	public Run(int closeSize) {
-		edgeDetection = new StructureElement(new boolean[][] {
-			new boolean[]{true, true, true},
-			new boolean[]{true, true, true},
-			new boolean[]{true, true, true},
-		}, 1, 1);
+	private final StructureElement justKeepTheScrew;
 
-		boolean[][] closeMask = new boolean[closeSize][closeSize];
-		for (int a = 0; a < closeMask.length; ++a) {
-			boolean[] array = closeMask[a];
+	public Run(int closeSize) {
+		edgeDetection = createCenteredSquare(3);
+		closeHoles = createCenteredSquare(closeSize);
+		justKeepTheScrew = createCenteredSquare(20);
+	}
+
+	private static StructureElement createCenteredSquare(int size) {
+		return new StructureElement(createSquareMask(size), size / 2, size / 2);
+	}
+
+	private static boolean[][] createSquareMask(int size) {
+		boolean[][] mask = new boolean[size][size];
+		for (int a = 0; a < mask.length; ++a) {
+			boolean[] array = mask[a];
 			for (int b = 0; b < array.length; ++b) {
 				array[b] = true;
 			}
 		}
-		closeHoles = new StructureElement(closeMask, closeSize/2, closeSize/2);
+		return mask;
 	}
-	
+
 	public void runEdgeDetection(ImageProcessor ip) {
 		DilateAndErode.invert(ip);
 		DilateAndErode.close(ip, closeHoles);
@@ -39,8 +45,10 @@ public class Run{
 		DilateAndErode.dilate(ip, other, edgeDetection);
 		DilateAndErode.xor(other, ip);
 	}
-	
+
 	public void runFindScrew(ImageProcessor ip) {
-		DilateAndErode.open(ip, closeHoles);//remove stray white pixels
+		DilateAndErode.invert(ip);
+		DilateAndErode.close(ip, closeHoles);
+		DilateAndErode.open(ip, justKeepTheScrew);
 	}
 }
